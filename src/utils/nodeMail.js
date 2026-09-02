3d2743540c5619bc519a4cfe28a6c23ba168f6b7
duttachandan <duttaankit917@gmail.com>
2026-09-02 17:25:52 +0530

    updated node mail

---
+++ src/utils/nodeMail.js
@@ -0,0 +1,10 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: "me@gmail.com",
+    }
+})
+
